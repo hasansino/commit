e429e33e4168b9eb39582abc529511efb4a8d30c
hasansino <4024450+hasansino@users.noreply.github.com>
2026-08-29 12:04:54 +0300

    fix(entrypoint): update command execution handling in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 
 # default command, expects 'commit' executable to be available in $PATH
 if [ "$1" = 'app' ]; then
-  exec commit "${@:2}"
+  shift
+  exec commit "$@"
 fi
 
 # if arbitrary command was passed, execute it instead of default one
